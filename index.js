1b0972d6eab2eea954c4afd68ff3b1dfdc2459b3
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2020-11-27 11:45:58 +0530

    Add initial website files

---
+++ index.js
@@ -0,0 +1,13 @@
+var img1 = Math.floor(Math.random() * 6) + 1;
+  	var img2 = Math.floor(Math.random() * 6) + 1;
+    document.getElementsByTagName('img')[0].src = "/home/system/Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/images/dice" + img1 + ".png";
+    document.getElementsByTagName('img')[1].src = "/home/system/Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/images/dice" + img2 + ".png";
+    if (img1 > img2) {
+    	document.getElementById("heading").innerHTML = "🚩Player 1 Wins!";
+    }
+    else if(img2 > img1){
+        document.getElementById("heading").innerHTML = "Player 2 Wins!🚩";
+    }
+    else{
+    	document.getElementById("heading").innerHTML = "Draw!";
+    }
